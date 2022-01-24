e8a4a8b029a6f71330b2c6e55f92e13e7f113f4c
Reginaldo de Araujo Oliveira <rao.corretor@gmail.com>
2022-01-24 16:05:49 -0300

    Primeiro Projeto com React
    Meu primeiro projeto React com:
    Node.JS (npm e yarn)
    Next.JS ( react, react-dom e next)
    VS Code

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Esse é o novo <a href="https://nextjs.org">Next.js app</a> do RAO!
         </h1>
 
         <p className={styles.description}>
